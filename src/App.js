009655bd3cdca44d1e5d81cef6d33d902310e3a8
Peter <petermurphy5672@gmail.com>
2023-01-15 15:45:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
+
+// export the App function (component) so that it may be imported in other files, i.e. index.js
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// render the App component in the 'root' element found in index.html
